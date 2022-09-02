40f74ac10334768a4f2c002162da9a888bfbf05f
Arian Racca <arianracca@gmail.com>
2022-09-02 08:00:23 -0300

    Version 1.0 ready

---
+++ README.md
@@ -0,0 +1,11 @@
+# Welcome to Video to GIF
+
+## Create your own gif from any video
+
+1. Write the extension of the video you want to convert.
+2. Write the name of the video file (without extension)
+3. Write the path to save the file wherever you want.
+
+I've noticed the GIF the program export is a bit large (1Mb for 1 second aprox).
+Next step on my work will be make it smaller and add a cut scene where you can extract
+just seconds from a video automatically.
--- VideoToGif.py
+++ VideoToGif.py
@@ -1,6 +1,40 @@
-from distutils import extension
+# QRcodeWebPages.py - Flexible QR code maker.
+# To run:
+# certifi==2022.6.15
+# charset-normalizer==2.1.1
+# colorama==0.4.5
+# decorator==4.4.2
+# idna==3.3
+# imageio==2.21.2
+# imageio-ffmpeg==0.4.7
+# moviepy==1.0.3
+# numpy==1.23.2
+# Pillow==9.2.0
+# proglog==0.1.10
+# requests==2.28.1
+# tqdm==4.64.0
+# urllib3==1.26.12
+
+__version__ = "1.0"
+
+import os
+from setuptools import extension
 from moviepy.editor import VideoFileClip
-ext = str(input("Qué formato tiene el video que quieres hacer gif? (ejemplo: mp4, avi...) "))
-videoName = str(input("Elije el nombre de tu video de Origen (en la carpeta del programa en ejecucion) "))
-videoClip = VideoFileClip(videoName+"."+ext)
-videoClip.write_gif(videoName+".gif")
+
+if __name__ == '__main__':
+    print("""--->> BIENVENIDO A VIDEO TO GIF <<---
+Con este programa podrás transformar un video en una imagen Gif animada.
+El video del cual generará el GIF debe encontrarse en la carpeta del programa,
+así podrá encontrarlo el programa cuando escribas su nombre.
+""")
+    ext = str(input("""Qué formato tiene el video de origen que
+quieres convertir en gif? (ejemplo: mp4, avi...)
+"""))
+    video_name = str(input("""Elige el nombre de tu video de Origen
+(en la carpeta del programa en ejecucion)
+"""))
+    folder = input("Ingrese la ruta de acceso a la Carpeta para Guardar el Archivo\n")
+    video_clip = VideoFileClip(video_name+"."+ext)
+    path_like_name = os.path.normpath(folder+"\\"+video_name+".gif")
+    video_clip.write_gif(path_like_name)
+    print("El archivo "+video_name+".gif fue guardado en la carpeta: "+folder)
